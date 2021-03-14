294611e1cdcad5f895521a8d4e2aed6e493dfd92
Simon Leier <simonleier@gmail.com>
2021-03-15 00:40:13 +0100

    Add iOS target

---
+++ dart-ffi/tools/build_ios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rustup target add aarch64-apple-ios
+
+cargo install cargo-lipo
+
+cargo lipo --targets aarch64-apple-ios x86_64-apple-ios --release
+
+mv "target/universal/release/libisar_core_dart_ffi.a" "libisar_ios.a"
