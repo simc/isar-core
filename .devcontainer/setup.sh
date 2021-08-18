8e01b22a5020b3334a180c6edb63b635d3fce57c
Simon Leier <simonleier@gmail.com>
2021-08-18 08:53:58 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -19,6 +19,3 @@ rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
 rustup component add clippy
 rustup component add clippy --toolchain nightly
-
-cargo install cargo-expand
-cargo install cargo-edit
