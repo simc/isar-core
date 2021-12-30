f995457b64e67ae1a16b665ec82529d8c42d2da3
Simon Leier <simonleier@gmail.com>
2021-12-30 23:26:48 +0200

    Implement UTF-16 word boundaries

--- dart-ffi/src/lib.rs
+++ dart-ffi/src/lib.rs
@@ -3,6 +3,7 @@
 
 use isar_core::error::{illegal_arg, Result};
 use std::ffi::CStr;
+use std::mem;
 use std::os::raw::c_char;
 
 #[macro_use]
@@ -34,3 +35,26 @@ unsafe impl Send for UintSend {}
 pub struct BoolSend(&'static mut bool);
 
 unsafe impl Send for BoolSend {}
+
+#[no_mangle]
+pub unsafe extern "C" fn isar_find_word_boundaries(
+    input: *const c_char,
+    length: *mut u32,
+) -> *mut u32 {
+    let str = from_c_str(input).unwrap();
+    let mut result = vec![];
+    for (offset, word) in str.unicode_word_indices() {
+        result.push(offset as u32);
+        result.push((offset + word.encode_utf16().count()) as u32);
+    }
+    result.shrink_to_fit();
+    length.write(result.len() as u32);
+    let result_ptr = result.as_mut_ptr();
+    mem::forget(result);
+    result_ptr
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn isar_free_word_boundaries(boundaries: *mut u32, length: u32) {
+    Vec::from_raw_parts(boundaries, length as usize, length as usize);
+}
