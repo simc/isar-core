c1aa8195f8f801e06eb5e1b35c64aee8276c33a2
Simon Leier <simonleier@gmail.com>
2022-01-13 00:41:19 +0100

    Fix build

--- dart-ffi/tools/build_desktop.sh
+++ dart-ffi/tools/build_desktop.sh
@@ -2,22 +2,22 @@
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
-    cargo build --target x86_64-unknown-linux-gnu --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+    cargo build --target x86_64-unknown-linux-gnu --release
     mv "target/release/libisar.so" "libisar_linux_x64.so"
     ;;
   darwin*)
     if [ "$1" = "x64" ]; then
       rustup target add x86_64-apple-darwin
-      cargo build --target x86_64-apple-darwin --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+      cargo build --target x86_64-apple-darwin --release
       mv "target/x86_64-apple-darwin/release/libisar.dylib" "libisar_macos_x64.dylib"
     else
       rustup target add aarch64-apple-darwin
-      cargo build --target aarch64-apple-darwin --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+      cargo build --target aarch64-apple-darwin --release
       mv "target/aarch64-apple-darwin/release/libisar.dylib" "libisar_macos_arm64.dylib"
     fi
     ;;
   *)
-    cargo build --target x86_64-pc-windows-msvc --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+    cargo build --target x86_64-pc-windows-msvc --release
     mv "target/release/isar.dll" "isar_windows_x64.dll"
     ;;
 esac
--- dart-ffi/tools/build_ios.sh
+++ dart-ffi/tools/build_ios.sh
@@ -4,6 +4,6 @@ rustup target add aarch64-apple-ios x86_64-apple-ios
 
 cargo install cargo-lipo
 
-cargo lipo --targets aarch64-apple-ios x86_64-apple-ios -Z build-std=std --release
+cargo lipo --targets aarch64-apple-ios x86_64-apple-ios --release
 
 mv "target/universal/release/libisar.a" "libisar_ios.a"
