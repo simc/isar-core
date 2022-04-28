060f18e14da048d49566f0d9652345ed6bcf1db1
Simon Leier <simonleier@gmail.com>
2022-04-28 11:10:16 +0200

    Universal binary for macos

--- dart-ffi/tools/build_macos.sh
+++ dart-ffi/tools/build_macos.sh
@@ -1,9 +1,5 @@
-if [ "$1" = "x64" ]; then
-  rustup target add x86_64-apple-darwin
-  cargo build --target x86_64-apple-darwin --release
-  mv "target/x86_64-apple-darwin/release/libisar.dylib" "libisar_macos_x64.dylib"
-else
-  rustup target add aarch64-apple-darwin
-  cargo build --target aarch64-apple-darwin --release
-  mv "target/aarch64-apple-darwin/release/libisar.dylib" "libisar_macos_arm64.dylib"
-fi
+rustup target add aarch64-apple-darwin x86_64-apple-darwin
+cargo build --target aarch64-apple-darwin --release
+cargo build --target x86_64-apple-darwin --release
+
+lipo "target/aarch64-apple-darwin/release/libisar.dylib" "target/x86_64-apple-darwin/release/libisar.dylib" -output "libisar_macos.dylib" -create
