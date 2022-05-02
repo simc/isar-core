210bb94f86fdff5a68993b4a98275c53f6fa0083
Simon Leier <simonleier@gmail.com>
2022-05-02 08:59:50 +0200

    Update build_ios.sh

--- dart-ffi/tools/build_ios.sh
+++ dart-ffi/tools/build_ios.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
 rustup target add aarch64-apple-ios x86_64-apple-ios
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target aarch64-apple-ios --release
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-apple-ios --release
 
-cargo install cargo-lipo
-
-cargo lipo --targets aarch64-apple-ios x86_64-apple-ios --release
-
-mv "target/universal/release/libisar.a" "libisar_ios.a"
+lipo "target/aarch64-apple-ios/release/libisar.a" "target/x86_64-apple-ios/release/libisar.a" -output "libisar_ios.a" -create
