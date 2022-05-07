f132794f1f4bc78c8d787c3811ec26c6f9f60b05
Simon Leier <simonleier@gmail.com>
2022-05-07 11:04:57 +0200

    disable panic abort for armv7

--- dart-ffi/tools/build_linux.sh
+++ dart-ffi/tools/build_linux.sh
@@ -1,9 +1,9 @@
 if [ "$1" = "x64" ]; then
   rustup target add target x86_64-unknown-linux-gnu
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
+  cargo +nightly build -C panic=abort -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
   mv "target/x86_64-unknown-linux-gnu/release/libisar.so" "libisar_linux_x64.so"
 else
   rustup target add aarch64-unknown-linux-gnu
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target aarch64-unknown-linux-gnu --release
+  cargo +nightly build -C panic=abort -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target aarch64-unknown-linux-gnu --release
   mv "target/aarch64-unknown-linux-gnu/release/libisar.so" "libisar_linux_arm64.so"
 fi
