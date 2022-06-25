b99d2c9d9b2bfa2ab42289c3ae63e8466e3b122f
Simon Leier <simonleier@gmail.com>
2022-06-25 20:03:57 +0200

    Update other.rs

--- app-dir/src/other.rs
+++ app-dir/src/other.rs
@@ -1,7 +1,7 @@
 pub fn get_dir() -> Option<String> {
-    dirs::config_dir()?.to_str()?.to_string()
+    Some(dirs::config_dir()?.to_str()?.to_string())
 }
 
 pub fn get_app_id() -> Option<String> {
-    String::new()
+    Some(String::new())
 }
