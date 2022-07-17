b9bbe0d5d6c741e46c32ac6186e3a59edb07a1f3
Simon Leier <simonleier@gmail.com>
2022-07-17 10:54:02 +0200

    Update build_android.sh

--- dart-ffi/tools/build_android.sh
+++ dart-ffi/tools/build_android.sh
@@ -9,7 +9,10 @@ else
     exit
 fi
 
-NDK=${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-"$ANDROID_SDK_ROOT/ndk"}}
+NDK="$ANDROID_HOME/ndk/21.4.7075529"
+if [ ! -d "$NDK" ]; then
+    NDK=${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-"$ANDROID_SDK_ROOT/ndk"}}
+fi
 COMPILER_DIR="$NDK/toolchains/llvm/prebuilt/$NDK_HOST_TAG/bin"
 export PATH="$COMPILER_DIR:$PATH"
 
@@ -22,28 +25,22 @@ export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$COMPILER_DIR/x86_64-linux-andr
 export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_AR="$COMPILER_DIR/armv7a-linux-androideabi-ar"
 export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$COMPILER_DIR/armv7a-linux-androideabi21-clang"
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$COMPILER_DIR/aarch64-linux-android-ar"
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$COMPILER_DIR/aarch64-linux-android21-clang"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$COMPILER_DIR/aarch64-linux-android29-clang"
 
 if [ "$1" = "x86" ]; then
   rustup target add i686-linux-android
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target i686-linux-android --release
+  cargo build --target i686-linux-android --release
   mv "target/i686-linux-android/release/libisar.so" "libisar_android_x86.so"
 elif [ "$1" = "x64" ]; then
   rustup target add x86_64-linux-android
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-linux-android --release
+  cargo build --target x86_64-linux-android --release
   mv "target/x86_64-linux-android/release/libisar.so" "libisar_android_x64.so"
 elif [ "$1" = "armv7" ]; then
   rustup target add armv7-linux-androideabi
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target armv7-linux-androideabi --release
+  cargo build --target armv7-linux-androideabi --release
   mv "target/armv7-linux-androideabi/release/libisar.so" "libisar_android_armv7.so"
 else
   rustup target add aarch64-linux-android
-  cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target aarch64-linux-android --release
+  cargo build --target aarch64-linux-android --release
   mv "target/aarch64-linux-android/release/libisar.so" "libisar_android_arm64.so"
 fi
-
-
-
-
-
-
