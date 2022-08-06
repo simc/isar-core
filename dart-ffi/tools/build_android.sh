799271247d57fd3835e5de01f625e9e9c7e74e30
Simon Leier <simonleier@gmail.com>
2022-08-07 00:07:04 +0200

    Fix Android build

--- dart-ffi/tools/build_android.sh
+++ dart-ffi/tools/build_android.sh
@@ -15,23 +15,12 @@ export PATH="$COMPILER_DIR:$PATH"
 
 echo "$COMPILER_DIR"
 
-export CC_i686_linux_android=$COMPILER_DIR/i686-linux-android21-clang
-export AR_i686_linux_android=$COMPILER_DIR/llvm-ar
 export CARGO_TARGET_I686_LINUX_ANDROID_LINKER=$COMPILER_DIR/i686-linux-android21-clang
 export CARGO_TARGET_I686_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
-
-export CC_x86_64_linux_android=$COMPILER_DIR/x86_64-linux-android21-clang
-export AR_x86_64_linux_android=$COMPILER_DIR/llvm-ar
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$COMPILER_DIR/x86_64-linux-android21-clang
 export CARGO_TARGET_X86_64_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
-
-export CC_armv7_linux_androideabi=$COMPILER_DIR/armv7a-linux-androideabi21-clang
-export AR_armv7_linux_androideabi=$COMPILER_DIR/llvm-ar
 export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$COMPILER_DIR/armv7a-linux-androideabi21-clang
 export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_AR=$COMPILER_DIR/llvm-ar
-
-export CC_aarch64_linux_android=$COMPILER_DIR/aarch64-linux-android21-clang
-export AR_aarch64_linux_android=$COMPILER_DIR/llvm-ar
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$COMPILER_DIR/aarch64-linux-android21-clang
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
 
