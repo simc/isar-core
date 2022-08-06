5f55c2f382a127e7a4ac1553ae25e47925493fc1
Simon Leier <simonleier@gmail.com>
2022-08-06 23:52:15 +0200

    Support link verification

--- dart-ffi/src/link.rs
+++ dart-ffi/src/link.rs
@@ -1,6 +1,7 @@
 use crate::txn::CIsarTxn;
 use isar_core::collection::IsarCollection;
 use isar_core::error::Result;
+use itertools::Itertools;
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_link(
@@ -69,6 +70,22 @@ pub unsafe extern "C" fn isar_link_update_all(
         {
             collection.unlink(txn, link_id, id, *target_id)?;
         }
+        Ok(())
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn isar_link_verify(
+    collection: &'static IsarCollection,
+    txn: &mut CIsarTxn,
+    link_id: u64,
+    ids: *const i64,
+    ids_count: u32,
+) -> i64 {
+    let ids = std::slice::from_raw_parts(ids, ids_count as usize);
+    let links = ids.iter().copied().tuples().collect_vec();
+    isar_try_txn!(txn, move |txn| -> Result<()> {
+        collection.verify_link(txn, link_id, &links)?;
         Ok(())
     })
 }
--- dart-ffi/tools/build_android.sh
+++ dart-ffi/tools/build_android.sh
@@ -9,23 +9,31 @@ else
     exit
 fi
 
-NDK="$ANDROID_HOME/ndk/21.4.7075529"
-if [ ! -d "$NDK" ]; then
-    NDK=${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-"$ANDROID_SDK_ROOT/ndk"}}
-fi
+NDK=${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-"$ANDROID_SDK_ROOT/ndk"}}
 COMPILER_DIR="$NDK/toolchains/llvm/prebuilt/$NDK_HOST_TAG/bin"
 export PATH="$COMPILER_DIR:$PATH"
 
 echo "$COMPILER_DIR"
 
-export CARGO_TARGET_I686_LINUX_ANDROID_AR="$COMPILER_DIR/i686-linux-android-ar"
-export CARGO_TARGET_I686_LINUX_ANDROID_LINKER="$COMPILER_DIR/i686-linux-android21-clang"
-export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$COMPILER_DIR/x86_64-linux-android-ar"
-export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$COMPILER_DIR/x86_64-linux-android21-clang"
-export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_AR="$COMPILER_DIR/armv7a-linux-androideabi-ar"
-export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$COMPILER_DIR/armv7a-linux-androideabi21-clang"
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$COMPILER_DIR/aarch64-linux-android-ar"
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$COMPILER_DIR/aarch64-linux-android29-clang"
+export CC_i686_linux_android=$COMPILER_DIR/i686-linux-android21-clang
+export AR_i686_linux_android=$COMPILER_DIR/llvm-ar
+export CARGO_TARGET_I686_LINUX_ANDROID_LINKER=$COMPILER_DIR/i686-linux-android21-clang
+export CARGO_TARGET_I686_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
+
+export CC_x86_64_linux_android=$COMPILER_DIR/x86_64-linux-android21-clang
+export AR_x86_64_linux_android=$COMPILER_DIR/llvm-ar
+export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$COMPILER_DIR/x86_64-linux-android21-clang
+export CARGO_TARGET_X86_64_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
+
+export CC_armv7_linux_androideabi=$COMPILER_DIR/armv7a-linux-androideabi21-clang
+export AR_armv7_linux_androideabi=$COMPILER_DIR/llvm-ar
+export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$COMPILER_DIR/armv7a-linux-androideabi21-clang
+export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_AR=$COMPILER_DIR/llvm-ar
+
+export CC_aarch64_linux_android=$COMPILER_DIR/aarch64-linux-android21-clang
+export AR_aarch64_linux_android=$COMPILER_DIR/llvm-ar
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$COMPILER_DIR/aarch64-linux-android21-clang
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR=$COMPILER_DIR/llvm-ar
 
 if [ "$1" = "x86" ]; then
   rustup target add i686-linux-android
